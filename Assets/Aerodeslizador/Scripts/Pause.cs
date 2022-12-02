19460a9ba7d794f730d89d768123d2fae28c0add
Juan Pablo Salazar Portilla <93442109+AnSaJu@users.noreply.github.com>
2022-12-02 08:11:18 -0500

    Pause menu

---
+++ Assets/Aerodeslizador/Scripts/Pause.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+
+    //[SerializeField] int sceneIndex;
+    [SerializeField] GameObject panelPause;
+
+    bool paused = false;
+    // Update is called once per frame
+
+    private void Awake()
+    {
+        Time.timeScale = 1;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+            //panelPause.SetActive(true);
+            
+        }
+    }
+
+    public void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            panelPause.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            panelPause.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void LoadNextScene(int sceneIndex)
+    {
+
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
